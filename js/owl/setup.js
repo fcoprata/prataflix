c3c4c6f1420dd5d27c33510954207718c81792c6
Francisco Prata <fcoprata@alu.ufc.br>
2021-07-31 11:09:48 -0300

    codeless improvements

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -25,3 +25,36 @@ function redirect(){
 function trailer(){
     window.open("https://youtu.be/4ake9I0-6vA");
 }
+function filme1(){
+    window.open("https://pt.wikipedia.org/wiki/Naruto#Naruto_Shippuden")
+}
+function filme2(){
+    window.open("https://pt.wikipedia.org/wiki/Narcos")
+}
+function filme3(){
+    window.open("https://pt.wikipedia.org/wiki/Joker_(filme)")
+}
+function filme4(){
+    window.open("https://pt.wikipedia.org/wiki/Vi%C3%BAva_Negra_(filme)")
+}
+function filme5(){
+    window.open("https://pt.wikipedia.org/wiki/Peaky_Blinders_(s%C3%A9rie_de_televis%C3%A3o)")
+}
+function filme6(){
+    window.open("https://pt.wikipedia.org/wiki/Rick_and_Morty")
+}
+function filme7(){
+    window.open("https://pt.wikipedia.org/wiki/Loki_(s%C3%A9rie_de_televis%C3%A3o)")
+}
+function filme8(){
+    window.open("https://pt.wikipedia.org/wiki/Mortal_Kombat_(filme_de_2021)")
+}
+function filme9(){
+    window.open("https://pt.wikipedia.org/wiki/Godzilla_vs._Kong")
+}
+function filme10(){
+    window.open("https://pt.wikipedia.org/wiki/Zack_Snyder%27s_Justice_League")
+}
+function git(){
+    window.open("https://github.com/fcoprata/cloneflix")
+}
